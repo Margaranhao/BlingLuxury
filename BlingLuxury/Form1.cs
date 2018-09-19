6079ea66ab37b3e6942fca615cce609d7a1e49f2
17 Wicho Ramos 17 <17 Wicho Ramos 17@17-RAMOS-17>
2018-09-19 11:32:58 -0500

    el chido

---
+++ BlingLuxury/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace BlingLuxury
+{
+
+    public partial class fmLogin : Form
+    {
+        
+        
+        public fmLogin()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //placeholder para el campo usuario
+            txtUser.GotFocus += new EventHandler(this.TextGotFocus);
+            txtUser.LostFocus += new EventHandler(this.TextLostFocus);
+
+            txtPass.GotFocus += new EventHandler(this.TextGotFocus1);
+            txtPass.LostFocus += new EventHandler(this.TextLostFocus1);
+
+        }
+
+        public void TextGotFocus(object sender, EventArgs e)
+        {
+            if(txtUser.Text == "usuario")
+            {
+                txtUser.Text = "";
+                txtUser.ForeColor = Color.Black;
+            }
+        }
+        public void TextGotFocus1(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "password")
+            {
+                txtPass.Text = "";
+                txtPass.ForeColor = Color.Black;
+            }
+        }
+
+        public void TextLostFocus(object sender, EventArgs e)
+        {
+            if(txtUser.Text == "")
+            {
+                txtUser.Text = "usuario";
+                txtUser.ForeColor = Color.Gray;
+            }
+        }
+        public void TextLostFocus1(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "password";
+                txtPass.ForeColor = Color.Gray;
+            }
+        }
+        BaseDeDatos d = new BaseDeDatos();
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (d.executecommand("select * from usuarios where usuario='" + txtUser.Text + "'and pass ='" + txtPass.Text + "'"));
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fmRegistro registro = new fmRegistro();
+            registro.Show();
+        }
+
+        private void btnSalir_Click_1(object sender, EventArgs e)
+        {
+            // Close();
+            Application.Exit();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
